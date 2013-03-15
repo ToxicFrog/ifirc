d44625ab6e6fa81d0f758a749aa4a965c0bfa23a
Ben Kelly <bk@ancilla.ca>
2013-03-15 19:23:23 -0400

    A little testing thingy

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "telnet://ifmud.port4000.com:4000/"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [saturnine "0.3-SNAPSHOT"]]
   :main ifirc.core)
--- src/ifirc/core.clj
+++ src/ifirc/core.clj
@@ -1,7 +1,17 @@
 (ns ifirc.core
-  (:gen-class))
+  (:gen-class)
+  (:use [saturnine.core]
+        [saturnine.handler]))
+
+(defhandler Test [host port]
+  "Given a host and port of a MUD to connect to, proxy IRC connections to that MUD, translating between them."
+  (connect [this] (println this (connection)))
+  (disconnect [this] (println this))
+  (upstream [this msg] (println this msg) (assoc this :server "aaaa"))
+  (downstream [this msg] (println this msg))
+  (error [this msg] (println this msg)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start-server 1234 :nonblocking :string :print (new Test "localhost" 1234)))
