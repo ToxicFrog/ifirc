8bfbb4f6553dd431050a647eda68c4648e757ab5
Ben Kelly <bk@ancilla.ca>
2013-03-16 00:49:24 -0400

    Add docstrings!

--- src/ifirc/mogrify.clj
+++ src/ifirc/mogrify.clj
@@ -3,7 +3,18 @@
         [saturnine.handler]
         [clojure.string :only [split-lines join]]))
 
-(defmacro defmogs [name & mogs]
+(defmacro defmogs
+  "Define a list of mogrifications to apply to a stream of lines of text.
+
+  Each mogrification has the form
+    (re [args] body)
+  and is compiled into a function f that when called as (f line), evaluates (re-matches re line), binds the result to
+  args, and evaluates body.
+
+  If re does not match line, it returns nil.
+
+  The resulting list is intended for use with (domogs) and (mogrify)."
+  [name & mogs]
   (defn defmog [regex args & body]
     `(fn [line#]
        (case (re-matches ~regex line#)
@@ -12,7 +23,12 @@
   (let [mogs (map (partial apply defmog) mogs)]
     `(def ~name [~@mogs])))
 
-(defn domogs [mogs line]
+(defn domogs
+  "Apply a list of mogrifications created with (defmogs) to a line of text.
+
+  Sequentially evaluates mogs to find the first one that matches line, and returns the result of that one. If none of
+  the mogs match, returns the unmodified line. Mogs are evaluated in the same order they were listed in (defmogs)."
+  [mogs line]
   (let [output (some #(% line) mogs)]
     (cond
       output output
