68e6f59c368843a760cf69c86886b847845bc3ff
Ben Kelly <bk@ancilla.ca>
2014-10-11 21:36:28 -0400

    Fix license info and add :uberjar profile in project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject ifirc "0.1.0"
   :description "A bridge between an IRC client and IFMUD"
   :url "telnet://ifmud.port4000.com:4000/"
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.301.0-deb34a-alpha"]
                  [com.taoensso/timbre "3.1.6"]]
-  :main ifirc.core)
+  :main ^:skip-aot ifirc.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
